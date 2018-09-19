150d7c1926387a322c4ed50416040867a0bf0cab
arifwi <43029510+arifwi@users.noreply.github.com>
2018-09-19 16:56:10 +0700

    update

--- getComputerList.php
+++ getComputerList.php
@@ -3,12 +3,21 @@
 include 'checkConnection.php';
 
 
-$queryResult =$connect->query("SELECT glpi_computers.id, glpi_computers.name,glpi_computers.computermodels_id as 'model_id',glpi_computermodels.name as 'model_name', glpi_computers.computertypes_id as 'type_id', glpi_computertypes.name as 'type_name'
+$queryResult =$connect->query("SELECT glpi_computers.id, glpi_computers.name,glpi_computers.computermodels_id as 'model_id',glpi_computermodels.name as 'model_name', 
+glpi_computers.computertypes_id as 'type_id',
+glpi_computertypes.name as 'type_name',
+glpi_computers.users_id 'userid',
+upper(glpi_users.name) as 'username', 
+upper(glpi_users.firstname) as 'firstname', 
+upper(glpi_users.realname) as 'lastname'
 FROM glpi_computers
 LEFT JOIN glpi_computermodels ON
 glpi_computers.computermodels_id = glpi_computermodels.id
 LEFT JOIN glpi_computertypes ON
-glpi_computers.computertypes_id = glpi_computertypes.id");
+glpi_computers.computertypes_id = glpi_computertypes.id
+LEFT JOIN glpi_users ON
+glpi_computers.users_id = glpi_users.id
+");
 
 $result=array();
 
