c1325980673e38cbd754828e0d5b07d664b5fb5c
WIBOWO Arif <Arif.WIBOWO@mane.com>
2018-09-20 16:28:03 +0700

    update

--- getComputerList.php
+++ getComputerList.php
@@ -7,9 +7,13 @@ $queryResult =$connect->query("SELECT glpi_computers.id, glpi_computers.name,glp
 glpi_computers.computertypes_id as 'type_id',
 glpi_computertypes.name as 'type_name',
 glpi_computers.users_id 'userid',
+glpi_computers.serial as 'sn',
+glpi_computers.otherserial as 'pn',
 upper(glpi_users.name) as 'username', 
 upper(glpi_users.firstname) as 'firstname', 
-upper(glpi_users.realname) as 'lastname'
+upper(glpi_users.realname) as 'lastname',
+glpi_entities.name as 'entities_name',
+glpi_locations.name as 'location_name'
 FROM glpi_computers
 LEFT JOIN glpi_computermodels ON
 glpi_computers.computermodels_id = glpi_computermodels.id
@@ -17,6 +21,10 @@ LEFT JOIN glpi_computertypes ON
 glpi_computers.computertypes_id = glpi_computertypes.id
 LEFT JOIN glpi_users ON
 glpi_computers.users_id = glpi_users.id
+LEFT JOIN glpi_entities ON
+glpi_computers.entities_id = glpi_entities.id
+LEFT JOIN glpi_locations on
+glpi_computers.locations_id = glpi_locations.id
 ");
 
 $result=array();
