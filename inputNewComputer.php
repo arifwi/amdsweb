fb34e2852dd139dd64ae9f9054ceb4594b60ae3f
arifwi <arifwibowoun@gmail.com>
2018-10-22 06:53:08 +0700

    updates
    add logs

--- inputNewComputer.php
+++ inputNewComputer.php
@@ -2,6 +2,7 @@
 
 include 'checkConnection.php';
 
+
 $name = $_POST['id'];
 $computerTypes_id = $_POST['type_id'];
 $computerModels_id =$_POST['model_id'];
@@ -24,17 +25,21 @@ while($fetchData=$queryResult->fetch_assoc()){
     $result[]=$fetchData;
 }
 while($fetchData=$getLastId->fetch_assoc()){
-    $last_id["last_id"];
+    $last_id=$fetchData["last_id"];
 }
 
 if(count($result)!=0){
     echo 'DUPLICATE ID DETECTED';
 }
 else{
-  $queryResult =$connect->query("INSERT INTO glpi_computers(name,entities_id,serial,locations_id,computermodels_id,computertypes_id, users_id, states_id) VALUES('$name','$entities_id','SN: $serial - PN: $otherSerial','$locations_id','$computerModels_id','$computerTypes_id','$users_id','1')");
-  $queryInputLog = $connect->query("INSERT INTO glpi_logs(itemtype,items_id,itemtype_link,linked_action,user_name,date_mod,id_search_option,old_value,new_value) VALUES
-  ('Computer','$last_id','0','20','$user_name',now(),'0','','')");
-  echo 'INPUT SUCCESSFULL';
+  if($connect->query("INSERT INTO glpi_computers(name,entities_id,serial,locations_id,computermodels_id,computertypes_id, users_id, states_id, date_mod) VALUES('$name','$entities_id','SN: $serial - PN: $otherSerial','$locations_id','$computerModels_id','$computerTypes_id','$users_id','1', now())")=== TRUE){
+      if($connect->query("INSERT INTO glpi_logs(itemtype,items_id,itemtype_link,linked_action,user_name,date_mod,id_search_option,old_value,new_value) VALUES
+      ('Computer','$last_id','0','20','$user_name',now(),'0','','')")==TRUE){
+        echo 'INPUT SUCCESSFULL';
+
+      }
+  }
+  
 }
 
 ?>
